2177108997ecbf4a73636eb3165961dbf6f16207
Erick Sant'Ana da Silva <ericksans199@outlook.com>
2025-07-21 20:27:25 -0300

    adição de estilo pro contador de itens pro carrinho

--- src/components/UserButtons.jsx
+++ src/components/UserButtons.jsx
@@ -4,8 +4,14 @@ import { faCartShopping, faUser } from '@fortawesome/free-solid-svg-icons';
 const UserButtons = () => {
   return (
     <div>
-      <button className='px-2'>
+      <button className='px-2 relative'>
         <FontAwesomeIcon icon={faCartShopping} />
+        <div
+          id='cart-amount'
+          className='absolute inline-flex items-center justify-center w-6 h-6 font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2'
+        >
+          3
+        </div>
       </button>
       <a>
         <FontAwesomeIcon className='px-2' icon={faUser} />
