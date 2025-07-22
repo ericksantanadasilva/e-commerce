455cf462f50f85b2d7ce30d7dc295ffa1332e395
Erick Sant'Ana da Silva <ericksans199@outlook.com>
2025-07-21 23:39:54 -0300

    criação de rotas para criar as novas paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import Header from './components/Header';
 import Home from './pages/HomePage/Home';
+import Checkout from './pages/CheckoutPage/checkout';
+import PurchaseHistory from './pages/PurchaseHistoryPage/PurchaseHistory';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Header />
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/checkout' element={<Checkout />} />
+        <Route path='/history' element={<PurchaseHistory />} />
+      </Routes>
     </>
   );
 }
