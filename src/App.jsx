4c7739c487e48e108e3686d79ce6218f95541b7c
Erick Sant'Ana da Silva <ericksans199@outlook.com>
2025-07-24 12:31:05 -0300

    refatoração do CartContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,20 @@
-import { useState } from 'react';
 import Header from './components/Header';
 import Home from './pages/HomePage/Home';
 import Checkout from './pages/CheckoutPage/checkout';
 import PurchaseHistory from './pages/PurchaseHistoryPage/PurchaseHistory';
 import { Routes, Route } from 'react-router-dom';
-import { CartContext } from './contexts/CartContext';
+import CartContextProvider, { CartContext } from './contexts/CartContext';
 
 function App() {
-  const [isCartOpen, setIsCartOpen] = useState(false);
-  const [cartItems, setCartItems] = useState({});
-
-  const removeFromCart = (productId) => {
-    const cartItemsCopy = { ...cartItems };
-    delete cartItemsCopy[productId];
-    setCartItems(cartItemsCopy);
-  };
-
-  const addToCart = (productId) => {
-    const updatedCart = {
-      ...cartItems,
-      [productId]: (cartItems[productId] ?? 0) + 1,
-    };
-    setCartItems(updatedCart);
-  };
-
-  const decreaseUnit = (productId) => {
-    if (cartItems[productId] > 1) {
-      const updatedCart = {
-        ...cartItems,
-        [productId]: cartItems[productId] - 1,
-      };
-      setCartItems(updatedCart);
-    } else {
-      removeFromCart(productId);
-    }
-  };
-
-  const toogleIsCartOpen = () => {
-    setIsCartOpen(!isCartOpen);
-  };
-
   return (
-    <CartContext.Provider
-      value={{
-        isCartOpen,
-        toogleIsCartOpen,
-        cartItems,
-        addToCart,
-        decreaseUnit,
-        removeFromCart,
-      }}
-    >
+    <CartContextProvider>
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/checkout' element={<Checkout />} />
         <Route path='/history' element={<PurchaseHistory />} />
       </Routes>
-    </CartContext.Provider>
+    </CartContextProvider>
   );
 }
 
---
+++ src/contexts/CartContext.jsx
@@ -0,0 +1,63 @@
+import { useState, createContext } from 'react';
+
+export const CartContext = createContext(null);
+
+export const getAmountOfItemsInCart = (cartItemsObj) => {
+  let amount = 0;
+  for (const productId in cartItemsObj) {
+    amount += cartItemsObj[productId];
+  }
+  return amount;
+};
+
+const CartContextProvider = ({ children }) => {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState({});
+
+  const toogleIsCartOpen = () => {
+    setIsCartOpen(!isCartOpen);
+  };
+
+  const removeFromCart = (productId) => {
+    const cartItemsCopy = { ...cartItems };
+    delete cartItemsCopy[productId];
+    setCartItems(cartItemsCopy);
+  };
+
+  const addToCart = (productId) => {
+    const updatedCart = {
+      ...cartItems,
+      [productId]: (cartItems[productId] ?? 0) + 1,
+    };
+    setCartItems(updatedCart);
+  };
+
+  const decreaseUnit = (productId) => {
+    if (cartItems[productId] > 1) {
+      const updatedCart = {
+        ...cartItems,
+        [productId]: cartItems[productId] - 1,
+      };
+      setCartItems(updatedCart);
+    } else {
+      removeFromCart(productId);
+    }
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        isCartOpen,
+        cartItems,
+        toogleIsCartOpen,
+        removeFromCart,
+        addToCart,
+        decreaseUnit,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartContextProvider;
