3a0bc83f3a7b4332eac935b65885586d3bad4389
Erick Sant'Ana da Silva <ericksans199@outlook.com>
2025-07-24 18:56:17 -0300

    criação de input generico

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { useLocation, Link } from 'react-router-dom';
 const Header = () => {
   const { pathname } = useLocation();
   const completeHeader = (
-    <header className='flex text-xl sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-8 py-4 items-end justify-between text-xl z-10'>
+    <header className='flex text-xl sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-8 py-4 items-end justify-between z-10'>
       <Link to='/'>
         <img className='h-16 px-2' src={logo} alt='Logo da Hashtag.' />
       </Link>
@@ -14,7 +14,7 @@ const Header = () => {
   );
 
   const simpleHeader = (
-    <header className='flex text-xl sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-8 py-4 items-end justify-between text-xl z-10'>
+    <header className='flex text-xl sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-8 py-4 items-end justify-between  z-10'>
       <Link to='/'>
         <img className='h-12 px-2' src={logo} alt='Logo da Hashtag.' />
       </Link>
--- src/pages/CheckoutPage/Checkout.jsx
+++ src/pages/CheckoutPage/Checkout.jsx
@@ -1,5 +1,28 @@
+import FormInput from '../../utils/FormInput';
+
 const Checkout = () => {
-  return <h1></h1>;
+  return (
+    <main className='bg-stone-200 h-[calc(100dvh-5rem)]'>
+      <p className='text-center text-2xl font-bold text-slate-950 pt-8'>
+        Finalizar compra
+      </p>
+      <form className='grid grid-rows-5 grid-cols-3 grid-flow-col gap-4 h-3/4 mt-5 px-8'>
+        <FormInput
+          fieldType='text'
+          fieldName='Nome'
+          id='name'
+          placeholder='John'
+          required
+        />
+        <p className='bg-blue-700'>elemento 1</p>
+        <p className='bg-blue-700'>elemento 2</p>
+        <p className='bg-blue-700'>elemento 3</p>
+        <p className='bg-blue-700'>elemento 4</p>
+        <p className='bg-blue-700'>elemento 5</p>
+        <p className='bg-blue-700'>elemento 6</p>
+      </form>
+    </main>
+  );
 };
 
 export default Checkout;
